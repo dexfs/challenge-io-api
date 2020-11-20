e6db893b7a3d7cbe16af03c93da16a74b9cd8f1c
André Santos <andre@dexdev.com.br>
2020-11-19 22:59:41 -0300

    chore: project config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  roots: ['<rootDir>/__tests__'],
+  roots: ['<rootDir>/src/__tests__'],
   collectCoverageFrom: [
     '!<rootDir>/src/main/**'
   ],
+  testPathIgnorePatterns: ['<rootDir>/src/__tests__/__utils.js'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '@root': '.',
+    '@app': '<rootDir>/app',
+    '@orm': '<rootDir>/src/app/orm',
+    '@bootstrap': '<rootDir>/src/bootstrap',
+    '@database': '<rootDir>/src/database',
+    '@config': '<rootDir>/src/config',
+    '@shared': '<rootDir>/src/shared'
+  }
 }
